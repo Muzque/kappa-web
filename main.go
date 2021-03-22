029469e23e95eaea510dcadc24b29c8dd86460c1
Muzque <zerosky1943@gmail.com>
2021-03-22 22:37:37 +0800

    feat(BE): use viper to set global config

--- main.go
+++ main.go
@@ -2,22 +2,18 @@ package main
 
 import (
 	"log"
-	"net/http"
 
 	"github.com/gin-contrib/static"
 	"github.com/gin-gonic/gin"
 
 	"kappa-web/handler"
+	"kappa-web/config"
 )
-
-func NotFound(c *gin.Context) {
-	c.HTML(http.StatusOK, "index.html", gin.H{
-		"title": "Homepage",
-	})
-}
 
 func main() {
 	log.Println("Initializing ApiServer...")
+	load()
+
 	router := gin.Default()
 	router.Use(static.Serve("/", static.LocalFile("./frontend/dist", false)))
 	router.GET("/ping", handler.Ping)
@@ -26,5 +22,10 @@ func main() {
 	{
 		api.GET("/ping", handler.Ping)
 	}
-	router.Run(":7777")
+	router.Run(":" + config.Val.Port)
+}
+
+
+func load() {
+	config.Init()
 }
